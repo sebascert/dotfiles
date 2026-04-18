303d20d971b132536ea1ceca4e870589d4b5adf3
Sebastian Certuche <sebascertuche@gmail.com>
2026-04-17 20:02:38 -0600

    Add completions to open-docc

--- dot_bash.d/env2/my-completition.sh
+++ dot_bash.d/env2/my-completition.sh
@@ -4,3 +4,23 @@ _clip_cmd_complete() {
 }
 
 complete -F _clip_cmd_complete clip
+
+_open_docc_completion() {
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    if [[ $COMP_CWORD -eq 1 ]]; then
+        local dirs=()
+        if [[ -d "$HOME/docc" ]]; then
+            while IFS= read -r -d '' d; do
+                dirs+=("$(basename "$d")")
+            done < <(find "$HOME/docc" -mindepth 1 -maxdepth 1 -type d -print0 2>/dev/null)
+        fi
+
+        COMPREPLY=( $(compgen -W "${dirs[*]} -h --help" -- "$cur") )
+    else
+        COMPREPLY=()
+    fi
+}
+
+complete -F _open_docc_completion open-docc.sh
